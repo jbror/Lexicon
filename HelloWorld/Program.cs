7edb2f25c56cb9cc90bc2c6d74f5245f9e2a3d4e
bror <reachingbror@gmail.com>
2025-03-19 14:01:32 +0100

    Just testing my setup

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Watching the videos givien by the instructor and following along with the code examples. Just setting up my GitHub and Visual Studio to get going.
+Console.WriteLine("Hello, World! My name is Bror and i'm doing ToK at Lexicox. Wish me luck!");
