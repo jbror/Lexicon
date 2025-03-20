5ca0d1384da6a6c7eb63a2379259aa8054118477
bror <reachingbror@gmail.com>
2025-03-20 11:38:54 +0100

    Creating new project and following the content from the video course

---
+++ Decisions/Program.cs
@@ -0,0 +1,4 @@
+﻿//Watching this video https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/the-if-decision-statement
+
+Console.WriteLine("Bror's Big Giveaway...");
+Console.WriteLine("Choosee");
