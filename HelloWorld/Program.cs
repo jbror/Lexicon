c6907ce70eae6dca3d0642a4c76ff555e5b65a72
bror <reachingbror@gmail.com>
2025-03-20 11:28:22 +0100

    Just playing and testing the syntax while watching the video. Now on to next!

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,8 @@
-﻿//Watching the videos givien by the instructor and following along with the code examples. Just setting up my GitHub and Visual Studio to get going.
+﻿//https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/understanding-your-first-c-program
+//Watching the videos givien by the instructor and following along with the code examples. Just setting up my GitHub and Visual Studio to get going.
 Console.WriteLine("Hello, World! My name is Bror and i'm doing ToK at Lexicox. Wish me luck!");
+//Console.ReadLine(); // This takes input and waits for the user to press enter before closing the console window
+
+Console.WriteLine("What is your favorite cake?");
+string myFavoriteCake = Console.ReadLine();
+Console.WriteLine("Best cake in the world is " + myFavoriteCake + "!");
