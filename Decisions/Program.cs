95011228fff74cedb5c4b01ae74c7f9711d94cb6
bror <reachingbror@gmail.com>
2025-03-21 15:37:23 +0100

    Adding code from the CourseVideo, BUP

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -1,4 +1,24 @@
 ﻿//Watching this video https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/the-if-decision-statement
 
+using System.ComponentModel.Design;
+
 Console.WriteLine("Bror's Big Giveaway...");
-Console.WriteLine("Choosee");
+Console.WriteLine("Choosee a door: 1, 2 or 3: ");
+string userValue = Console.ReadLine();
+
+if (userValue == "1")
+{
+    string message = "You won a kexchocklad!";
+    Console.WriteLine(message);
+}
+else if (userValue == "2")
+{
+    string message = "You got a brick to the face!";
+    Console.WriteLine(message);
+}
+else if (userValue== "3")
+{
+    string message = "You get a banana!";
+    Console.WriteLine(message);
+}
+Console.ReadLine();
