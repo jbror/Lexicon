e0a9026e1ecc14d804ee50b75df1abceb11c10c6
bror <reachingbror@gmail.com>
2025-03-21 08:53:36 +0100

    Still working on getting the syntax and just wrote this to code something stupid while trying things

--- RedOrBlueKaka/Program.cs
+++ RedOrBlueKaka/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ToK Lexicon , Bror Johansson
+
+Console.Write("Vad är ditt namn? ");
+string namn = Console.ReadLine();
+
+Console.Write("Vill du ta den röda kakan " + namn + " eller den blåa kakan? (ange röd eller blå för att välja) \n");
+string val = Console.ReadLine().ToLower();
+
+if (val == "röd")
+{
+    Console.WriteLine(namn + " IS now blessed by the gods at Lexicons empire! Eat the cake " + namn + " and kneel before Jonathan, Håkan, Pontus next time you see them.");
+    Console.WriteLine("31 mars it all begins. Hallelujah!");
+}
+else if (val == "blå")
+{
+    Console.WriteLine("Vem fan äter den blåa kakan? Se Matrix och prova igen!");
+}
+else
+{
+    Console.WriteLine("Du valde inget alls, inget jag ser som giltligt i alla fall. Hejdå!"); //Om inga giltliga val görs! 
+}
