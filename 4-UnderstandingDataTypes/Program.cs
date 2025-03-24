22681f04f30eb6a2e687b706b34f050b37c87a10
bror <reachingbror@gmail.com>
2025-03-24 01:41:09 +0100

    Creating next project

---
+++ 4-UnderstandingDataTypes/Program.cs
@@ -0,0 +1,4 @@
+﻿//From the ToK material at Lexicon
+//This is the video related to this project https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/understanding-data-types-and-variables
+
+Console.WriteLine("Hej Hello Lexicon!");
