c34a1fc86af094468555ea77c5971fcef8ac9c6f
bror <reachingbror@gmail.com>
2025-03-24 17:00:25 +0100

    Some code, working on this on my laptop with vscode, bup

--- 4-UnderstandingDataTypes/Program.cs
+++ 4-UnderstandingDataTypes/Program.cs
@@ -2,3 +2,9 @@
 //This is the video related to this project https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/understanding-data-types-and-variables
 
 Console.WriteLine("Hej Hello Lexicon!");
+
+
+int x = 7;
+int y = x + 3;
+Console.WriteLine(x);
+Console.WriteLine(y);
