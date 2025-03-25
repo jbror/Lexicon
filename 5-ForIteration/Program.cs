fd68d86618bf9f5bd46ccece55e2f28ae5cdb3b3
bror <reachingbror@gmail.com>
2025-03-25 01:48:23 +0100

    Just creating project for the code that goes here

---
+++ 5-ForIteration/Program.cs
@@ -0,0 +1,4 @@
+﻿//On Lexicons Course
+//Doing this video https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/for-iteration-statement
+
+Console.WriteLine("Hello Sweden and Hello Lexicon!");
