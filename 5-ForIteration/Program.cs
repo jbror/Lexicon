fb54a58342c2c75e7ed90ca224483546d33d505b
bror <reachingbror@gmail.com>
2025-03-27 12:21:47 +0100

    Removed top-level-statements in this and just added some code from the video

--- 5-ForIteration/Program.cs
+++ 5-ForIteration/Program.cs
@@ -1,4 +1,29 @@
 ﻿//On Lexicons Course
 //Doing this video https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/for-iteration-statement
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello Sweden and Hello Lexicon!");
+namespace _5ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven, yes!");
+                    break;
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
