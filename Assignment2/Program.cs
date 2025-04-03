7161938414f6c51aeb98481abb26c49d52ba3835
bror <reachingbror@gmail.com>
2025-04-03 21:56:13 +0200

    Skeleton

---
+++ Assignment2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Uppgift 2
+Console.WriteLine("Hello, World!");
+var running = true;
+while (running)
+{
+    Console.WriteLine("You are now at the menu. Enter a number: ");
+    var userInput = Console.ReadLine();
+    switch (userInput)
+    {
+        case "0":
+            running = false;
+            break;
+
+        default:
+            Console.WriteLine("Invalid input!");
+            break;
+
+    }
+}
