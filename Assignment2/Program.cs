108bbe59c5f1559ef1b275dae9ee0f45fc5ed3a2
bror <reachingbror@gmail.com>
2025-04-07 08:41:02 +0200

    Adding changes from my laptop, draft atm

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,9 +1,12 @@
 ﻿//Uppgift 2
+using System.Reflection.Metadata;
+
 Console.WriteLine("Hello, hi and welcome to your local cinema!");
+Console.WriteLine("\nYou are now at the main menu. Please help the lovely developers by testing our new software.\nMake a choice by entering a number");
 var running = true;
 while (running)
 {
-    Console.WriteLine("\nYou are now at the main menu. Please help the lovely developers by testing our new software.\nMake a choice by entering a number");
+   // Console.WriteLine("\nYou are now at the main menu. Please help the lovely developers by testing our new software.\nMake a choice by entering a number");
     Console.WriteLine();
     Console.WriteLine("Press 1 for purchasing tickets");
     Console.WriteLine("Press 2 to enter your favorite word and see it be repeated back to you ten times! ");
@@ -40,25 +43,16 @@ void CalculateCinemaCost()
 {
 
     Console.WriteLine("This is case 1");
-    Console.WriteLine("Enter number of persons: ");
-    int numberOfPersons;
-    while (!int.TryParse(Console.ReadLine(), out numberOfPersons))
-    {
-        Console.WriteLine("Invalid input! Only numbers.");
-    }
+    var numberOfPersons = UserInputConvert("Enter number of persons: ");
 
     Console.WriteLine($"Okay, you are {numberOfPersons} persons.");
     var customerAges = new List<int>();
     var range = Enumerable.Range(1, numberOfPersons);
     foreach (var i in range)
     {
-        Console.WriteLine($"Enter age of person {i}:");
-        int age;
-        while (!int.TryParse(Console.ReadLine(), out age))
-        {
-            Console.WriteLine("Invalid input! Only numbers.");
-        }
-        customerAges.Add(age);
+        //Console.WriteLine($"Enter age of person {i}:");
+        var person_age = UserInputConvert($"Enter age of person {i}:");
+        customerAges.Add(person_age);
     }
     var totalPrice = new List<int>();
     for (int i = 0; i < customerAges.Count; i++)
@@ -86,6 +80,21 @@ void CalculateCinemaCost()
 
 
     }
+    int UserInputConvert(string prompt)
+    {
+        Console.WriteLine(prompt);
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Invalid input! Only numbers.");
+        }
+        return number;
+    }
+     //UserInputConvert("Enter number: ");
+
+
+
+
 }
 
 
